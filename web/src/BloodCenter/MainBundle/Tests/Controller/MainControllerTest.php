9498f64a18db9bd0140e553d2ef3427fe882549c
Katalyzator <web.coder96@gmail.com>
2016-03-16 13:07:03 +0600

    Temporary Template installed

---
+++ web/src/BloodCenter/MainBundle/Tests/Controller/MainControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BloodCenter\MainBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+    }
+
+}
